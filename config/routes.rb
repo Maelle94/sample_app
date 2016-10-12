b88575a4b0c8fc130940bd409d109b100cc64184
Maelle94 <senma94@gmail.com>
2016-10-12 13:40:40 +0200

    adding

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 
   get  'static_pages/about'
 
-  root 'application#hello'
+  get  'static_pages/contact'
+
+  #root 'application#hello'
+
+  root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
